665de43fe58767fa9b9b6de79b6796ab6491f5fd
daria <m2403815@edu.misis.ru>
2026-01-28 16:24:19 +0300

    adding constants

---
+++ internal/constants/errors.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	ErrCodeTeamExists  = "TEAM_EXISTS"
+	ErrCodePRExists    = "PR_EXISTS"
+	ErrCodePRMerged    = "PR_MERGED"
+	ErrCodeNotAssigned = "NOT_ASSIGNED"
+	ErrCodeNoCandidate = "NO_CANDIDATE"
+	ErrCodeNotFound    = "NOT_FOUND"
+)
